d6255d9598ccf86b6a9dcd283c1ae4f5c6f8751d
lemonlyk <lemon_lyk@icloud.com>
2017-11-24 15:37:59 +0800

    vue render

--- routes/posts.js
+++ routes/posts.js
@@ -5,7 +5,12 @@ var router = express.Router();
 /* GET posts page. */
 router.get('/', function(req, res, next) {
     //调用posts页，并传递title参数
-  res.render('posts', { title: 'Get post page',postsList:['文章1','文章2','文章3'] });
+  res.render('posts', { title: 'Get post page' });
 });
+
+/* GET 获取postsList数据 */
+router.get('/list', function(req, res, next) {
+    res.json({postsList: ['文章1', '文章2', '文章3'] });
+  });
 
 module.exports = router;
