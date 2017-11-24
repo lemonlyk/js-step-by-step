eec40ea61a5aa789bca384156e4f8826d8f7f155
lemonlyk <lemon_lyk@icloud.com>
2017-11-24 14:16:17 +0800

    custom ejs templet

--- routes/posts.js
+++ routes/posts.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET posts page. */
 router.get('/', function(req, res, next) {
     //调用posts页，并传递title参数
-  res.render('posts', { title: 'Get post page' });
+  res.render('posts', { title: 'Get post page',postsList:['文章1','文章2','文章3'] });
 });
 
 module.exports = router;
