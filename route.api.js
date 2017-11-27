f1250bf5540e07e2ae27eb4358b735e0a2d060cb
lemonlyk <lemon_lyk@icloud.com>
2017-11-27 09:39:28 +0800

    mongodb use

--- route.api.js
+++ route.api.js
@@ -1,7 +1,7 @@
 //获得router对象
 var express = require('express');
 var router = express.Router();
-
+var PostModel = require('./models/post');
 
 /* GET users listing. */
 router.get('/users', function(req, res, next) {
@@ -9,13 +9,32 @@ router.get('/users', function(req, res, next) {
   });
 /* GET posts lists */
 router.get('/posts', function(req, res, next) {
-    res.json({postsList: ['文章1', '文章2', '文章3']});
+    //res.json({postsList: ['文章1', '文章2', '文章3']});
+    PostModel.find({}, {}, function (err, posts) {
+      if (err) {
+        res.json({ success: false });
+        return;
+      }
+  
+      res.json({ success: true, postsList: posts });
+    });
   })
 
   /* POST posts */
 router.post('/posts/create', function (req, res, next) {
     var title = req.body.title;
     var content = req.body.content;
-    res.send({title, content}); // 收到数据后，又把数据返回给了请求方
+
+    var post = new PostModel();
+    post.title = title;
+    post.content = content;
+    post.save(function (err, doc) {
+      if(err)
+      {
+        res.json({success:false,errObj:err});
+      }
+      res.json({success: true});
+    });
+    //res.send({title, content}); // 收到数据后，又把数据返回给了请求方
   });
   module.exports = router;
--- route.page.js
+++ route.page.js
@@ -1,6 +1,7 @@
 //获得router对象
 var express = require('express');
 var router = express.Router();
+var PostModel = require('./models/post');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -17,5 +18,14 @@ router.get('/posts', function(req, res, next) {
 router.get('/posts/create', function(req, res, next) {
     res.render('create');
   });
+
+  /* GET posts show page. */
+router.get('/posts/show', function (req, res, next) {
+  var id = req.query.id;
+//console.log("id="+id);
+  PostModel.findOne({_id: id}, function (err, post) {
+    res.render('show', {post});
+  });
+});
 
 module.exports = router;
